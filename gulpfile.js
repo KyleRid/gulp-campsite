97b096030ba3491d5e4afc4ebefbf80efd04fd37
Vladimir Agapov <kyleridrus@gmail.com>
2020-07-05 21:37:47 +0300

    add TODO's

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,10 @@ const gulpfile = require('./gulpfile.class');
 
 exports.dev   = gulpfile.dev();
 exports.build = gulpfile.build();
+
+// TODO: add tests
+// TODO: add support of js/ts toggle
+// TODO: add support of scss/sass syntax toggle
+// TODO: configure ts linter
+// TODO: configure SASS linter
+// TODO: add support of pug/html
